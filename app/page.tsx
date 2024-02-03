2ac15f0922149fadda796beaf339c7dd229d55be
dehluizsantos <and_consul@outlook.com>
2024-02-03 16:12:58 -0300

    feat(structure):admin

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
+export default async function Index() {
+  const cookieStore = cookies();
+
+  const canInitSupabaseClient = () => {
+    // This function is just for the interactive tutorial.
+    // Feel free to remove it once you have Supabase connected.
+    try {
+      debugger;
+      createClient(cookieStore);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  };
+
+  const isSupabaseConnected = canInitSupabaseClient();
+
+  return (
+    <div>
+      <h1>Home page</h1>
+    </div>
+  );
+}
